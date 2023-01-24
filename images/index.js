518cc750a5a87f46ffcc541ebfedd89c1d7c0d7e
Oluniyisrael <israeloluniyi2004@gmail.com>
2023-01-24 11:37:09 +0100

    latest commit

---
+++ images/index.js
@@ -0,0 +1,5 @@
+var button1 = document.getElementById('portfolioBut');
+var button2 = document.getElementById('currencyBut');
+button1.addEventListener('click',()=>{
+    window.document.location.href = "https://oluniyi-port.netlify.app/"
+})
