70a9d686aec4c28cf6747d0d040ed29e166b21b3
Oluniyisrael <israeloluniyi2004@gmail.com>
2023-01-24 11:48:54 +0100

    I fink im finished

---
+++ index.js
@@ -0,0 +1,12 @@
+var button1 = document.getElementById('portfolioBut');
+var button2 = document.getElementById('currencyBut');
+button1.addEventListener('click',()=>{
+    document.location.href = "https://oluniyi-port.netlify.app/"
+})
+// document.getElementById('portfolioBut').addEventListener('click',()=>{
+//     document.location.href = "https://www.google.com"
+// })
+
+// document.getElementById('portfolioBut').addEventListener('click',()=>{
+//     document.location.href = "https://oluniyi-port.netlify.app/"
+// })
