d2e4f4ae2458d166ac6085ae6f846a7a20164031
Oluniyisrael <israeloluniyi2004@gmail.com>
2023-01-24 15:34:21 +0100

    Adding some finishing touches

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 var button1 = document.getElementById('portfolioBut');
 var button2 = document.getElementById('gitLink');
+var button3 = document.getElementsByClassName('cardButton')[2];
+var button4 = document.getElementsByClassName('cardButton')[3];
 button1.addEventListener('click',()=>{
     document.location.href = "https://oluniyi-port.netlify.app/"
 })
 button2.addEventListener('click',()=>{
     document.location.href = "https://github.com/Oluniyisrael/webdesignPortfolio"
+})
+button3.addEventListener('click',()=>{
+    document.location.href += "notYetDone.html"
+})
+button4.addEventListener('click',()=>{
+    document.location.href += "notYetDone.html"
 })
 // document.getElementById('portfolioBut').addEventListener('click',()=>{
 //     document.location.href = "https://www.google.com"
