5c1ddaf5493ad5d96f1af244c097caac140ddb02
Oluniyisrael <israeloluniyi2004@gmail.com>
2023-02-02 16:15:30 +0100

    Pushing the currency to my portfolio

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var button1 = document.getElementById('portfolioBut');
 var button2 = document.getElementById('gitLink');
-var button3 = document.getElementsByClassName('cardButton')[2];
-var button4 = document.getElementsByClassName('cardButton')[3];
+var button3 = document.getElementById('currencyBtn');
+var button4 = document.getElementById('gitLinkCur');
+var button5 = document.getElementsByClassName('cardButton')[4];
+var button6 = document.getElementsByClassName('cardButton')[5];
 button1.addEventListener('click',()=>{
     document.location.href = "https://oluniyi-port.netlify.app/"
 })
@@ -9,18 +11,17 @@ button2.addEventListener('click',()=>{
     document.location.href = "https://github.com/Oluniyisrael/webdesignPortfolio"
 })
 button3.addEventListener('click',()=>{
-    document.location.href += "notYetDone.html"
+    document.location.href = "https://izzyconverter.netlify.app/"
 })
 button4.addEventListener('click',()=>{
+    document.location.href = "https://github.com/Oluniyisrael/currencyConverter"
+})
+button5.addEventListener('click',()=>{
     document.location.href += "notYetDone.html"
 })
-// document.getElementById('portfolioBut').addEventListener('click',()=>{
-//     document.location.href = "https://www.google.com"
-// })
-
-// document.getElementById('portfolioBut').addEventListener('click',()=>{
-//     document.location.href = "https://oluniyi-port.netlify.app/"
-// })
+button6.addEventListener('click',()=>{
+    document.location.href += "notYetDone.html"
+})
 function scrollToHome() {
     var Dest =document.getElementById('landingPage');
     Dest.scrollIntoView();
@@ -29,5 +30,3 @@ function scrollToWorks() {
     var Dest =document.getElementById('works');
     Dest.scrollIntoView();
 }
-// var home = document.getElementById('liHome')
-// home.addEventListener('click', scrollTov())
